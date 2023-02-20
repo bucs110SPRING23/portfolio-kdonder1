f13861008d2a08e1616293ca8759a1038aa7d80e
Kristina Donders <kdonder1@binghamton.edu>
2023-02-20 17:47:38 -0500

    commit

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -6,6 +6,7 @@ for _ in range(3):
     guess = int(input("Guess a number between 1 and 10: "))
     if guess == number:
         print("Correct!")
+        break
     elif guess > number:
         print("Too High!, Guess again!")
     else:
