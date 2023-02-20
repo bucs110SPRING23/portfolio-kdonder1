35fb036e66c18fef6e7d76d97b0959369d578a2c
Kristina Donders <kdonder1@binghamton.edu>
2023-02-20 16:31:50 -0500

    zztop

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,12 +1,76 @@
-import turtle
+import turtle #1. import modules
 import random
+import pygame
+import math
 
 #Part A
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
+window = turtle.Screen() # 2.  Create a screen
+window.bgcolor('lightblue')
+
+michelangelo = turtle.Turtle() # 3.  Create two turtles
+leonardo = turtle.Turtle()
+michelangelo.color('orange')
+leonardo.color('blue')
+michelangelo.shape('turtle')
+leonardo.shape('turtle')
+
+michelangelo.up() # 4. Pick up the pen so we don’t get lines
+leonardo.up()
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
+## 5. Your PART A code goes here
+## RACE 1:
+michelangelo.speed(1)
+leonardo.speed(1)
+
+michelangelo.down() # 4. Pick up the pen so we don’t get lines
+leonardo.down()
+x = random.randrange(1,100)
+leonardo.forward(x)
+michelangelo.forward(x)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
+## RACE 2:
+y = random.randrange(1,10)
+z = random.randrange(1,10)
+for i in range(10):
+    leonardo.forward(y)
+    michelangelo.forward(z)
+
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 
-#Part B
+
+
+
+
+# PART B - complete part B here-- I HAVE NO CLUE WHATS GOING ON
+pygame.init()
+window = pygame.display.set_mode()
+
+screen_size = window.get_size()
+
+side_length = 350
+xpos = screen_size[0] / 2
+ypos = screen_size[1] / 2
+
+window.fill('deeppink3')
+shapelist = [3, 4, 6, 20, 100, 360]
+for num_sides in shapelist:
+    points = []
+    for i in range(num_sides):
+        angle = 360/num_sides
+        radians = math.radians(angle * i)
+        x = xpos + side_length * math.cos(radians)
+        y = ypos + side_length * math.sin(radians)
+        points.append([x, y])
+        ## surface, color, points, width
+    pygame.draw.polygon(window, 'pink', points, 0)
+    pygame.display.flip()
+    pygame.time.wait(1800)
+    window.fill('deeppink3')
+
+## window.exitonclick()
