896fe70a6ce675197cd3f39858f99deefc601f96
Kristina Donders <kdonder1@binghamton.edu>
2023-02-20 17:44:11 -0500

    guess commit

---
+++ ch03/exercises/guess.py
@@ -0,0 +1,12 @@
+import random
+
+number = random.randrange(11)
+
+for _ in range(3):
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess == number:
+        print("Correct!")
+    elif guess > number:
+        print("Too High!, Guess again!")
+    else:
+        print("Too Low! Guess again!")
---
+++ notes/febtwen.py
@@ -0,0 +1,35 @@
+## review of Boolean Functions
+
+if True:
+    print("True")
+
+if 6 < 5:
+    print("6 is less than 5, everything is broken")
+elif 6 == 5:
+    print ("math is still broken")
+else:
+    print("math is correct")
+
+x = 'a'
+# only one condition will execute
+if x < 'y':
+    print("yup")
+elif x < 'm': # optional
+    print("more yup")
+else: # optional
+    print("nope")
+
+
+
+
+## events
+
+## operating system handles events
+## your program -> OS: anything happening?
+
+## OS => event
+## type of event == operation
+# -click
+# x
+
+# connect actions to algorithms
