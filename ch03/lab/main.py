c7ba79bf02ecfe96dfde39445a94f0c4537de81c
Kristina Donders <kdonder1@binghamton.edu>
2023-02-27 14:27:36 -0500

    target practice

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,76 +1,48 @@
-import turtle #1. import modules
 import random
 import pygame
 import math
 
-#Part A
-window = turtle.Screen() # 2.  Create a screen
-window.bgcolor('lightblue')
-
-michelangelo = turtle.Turtle() # 3.  Create two turtles
-leonardo = turtle.Turtle()
-michelangelo.color('orange')
-leonardo.color('blue')
-michelangelo.shape('turtle')
-leonardo.shape('turtle')
-
-michelangelo.up() # 4. Pick up the pen so we don’t get lines
-leonardo.up()
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
-
-## 5. Your PART A code goes here
-## RACE 1:
-michelangelo.speed(1)
-leonardo.speed(1)
-
-michelangelo.down() # 4. Pick up the pen so we don’t get lines
-leonardo.down()
-x = random.randrange(1,100)
-leonardo.forward(x)
-michelangelo.forward(x)
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
-
-## RACE 2:
-y = random.randrange(1,10)
-z = random.randrange(1,10)
-for i in range(10):
-    leonardo.forward(y)
-    michelangelo.forward(z)
-
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
-
-
-
-
-
-
-# PART B - complete part B here-- I HAVE NO CLUE WHATS GOING ON
 pygame.init()
-window = pygame.display.set_mode()
+window = pygame.display.set_mode([864, 864])
+window.fill('darkblue')
 
-screen_size = window.get_size()
+screensize = window.get_size()
+print(screensize)
+length = screensize[0]
+width = screensize[1]
 
-side_length = 350
-xpos = screen_size[0] / 2
-ypos = screen_size[1] / 2
+## PART A: Screen + Dartboard
+# circle: surface, color, center, radius
+pygame.draw.circle(window, 'red', [length / 2, width / 2], width / 2)
+pygame.draw.circle(window, 'black', [length / 2, width / 2], width / 2, 4)
+pygame.draw.circle(window, 'white', [length / 2, width / 2], width / 2 - 120)
+pygame.draw.circle(window, 'red', [length / 2, width / 2], width / 2 - 240)
+pygame.draw.circle(window, 'white', [length / 2, width / 2], width / 2 - 360)
 
-window.fill('deeppink3')
-shapelist = [3, 4, 6, 20, 100, 360]
-for num_sides in shapelist:
-    points = []
-    for i in range(num_sides):
-        angle = 360/num_sides
-        radians = math.radians(angle * i)
-        x = xpos + side_length * math.cos(radians)
-        y = ypos + side_length * math.sin(radians)
-        points.append([x, y])
-        ## surface, color, points, width
-    pygame.draw.polygon(window, 'pink', points, 0)
+# line(surface, color, start_pos, end_pos)
+pygame.draw.line(window, 'black', [length / 2, 0], [length / 2, width], 3)
+pygame.draw.line(window, 'black', [0, width / 2], [length, width / 2], 3)
+
+
+## PART B: Dart throwing
+# Does the dart land within the circle?
+print(width / 2)
+
+for i in range(10):
+    x = random.randrange(int(length))
+    y = random.randrange(int(width))
+    distance_from_center = math.hypot(x-length/2, y-width/2) #the distance formula
+    is_in_circle = distance_from_center <= width/2 #screen width
+    if is_in_circle:
+        pygame.draw.circle(window, 'green', [x, y], 6)
+        print("passes", distance_from_center)
+    else:
+        pygame.draw.circle(window, 'orange', [x, y], 6)
+        print("doesn't pass", distance_from_center)
     pygame.display.flip()
-    pygame.time.wait(1800)
-    window.fill('deeppink3')
+    pygame.time.wait(300)
 
-## window.exitonclick()
+pygame.display.flip()
+pygame.time.wait(3000)
+
+# window.exitonclick()
