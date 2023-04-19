703a48c21142f5772f3fceda5333ed6b0fe857fa
Kristina Donders <kdonder1@binghamton.edu>
2023-04-19 17:20:43 -0400

    c

--- notes/snowman.py
+++ notes/snowman.py
@@ -1,5 +1,12 @@
 import pygame
 
-class Snowman:
+class Snowman(pygame.sprite.Sprite):
     
-    def __init__(self, x, y)
+    ## makes a a backsplash for code to be built on
+    def __init__(self, x, y, img = "assets.snowman.png")
+        super().__init__()
+
+        self.image = pygame.image.load
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
